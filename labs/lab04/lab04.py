bb37888c2ce8948e5c38f9b363adf4b176cb72ac
Hugo0713 <hugo0713@sjtu.edu.cn>
2024-09-28 20:12:29 +0800

    Refactor .ok_messages and lab04.py files

--- labs/lab04/lab04.py
+++ labs/lab04/lab04.py
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {quotient: [divisor for divisor in divisors if (divisor % quotient == 0)] for quotient in quotients}
 
 
 def buy(required_fruits, prices, total_amount):
